d50174c7e544c137a71bb73622c6ebe439cbe93b
Nguyen Phuong Tan <tannguyen.10102004@gmail.com>
2025-04-12 12:17:37 +0700

    update(index.js): change version

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
   // res.send('Hello World change codee to Render ECS Task Definition esc-task.json!, fix AmazonECS_FullAccess policy to AmazonECS_FullAccess policy')
   res.json({
     message: 'Hello World',
-    version: '1.0.0',
+    version: '2.0.0',
     description: 'This is a sample application running on AWS ECS using Node.js and Express. It demonstrates how to deploy a simple web application on AWS ECS using Fargate launch type.  change codee to Render ECS Task Definition esc-task.json!, fix AmazonECS_FullAccess policy to AmazonECS_FullAccess policy',
   })
 })
